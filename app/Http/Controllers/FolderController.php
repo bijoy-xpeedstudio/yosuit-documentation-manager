eb10a312f41fdad0bc2745f9fccbf190e460f771
Hussain Mahamud <mahamud@xpeedstudio.com>
2023-09-19 15:15:59 +0600

    Folder Create,Update,Destroy Added

--- app/Http/Controllers/FolderController.php
+++ app/Http/Controllers/FolderController.php
@@ -39,25 +39,22 @@ class FolderController extends Controller
     public function store(FolderRequest $request)
     {
         $request_time = date('y-m-d h:i:s');
-        // $request = $request->validated();
-        // try {
-        //     $folder = new Folder();
-        //     $folder->parent_id = $request['parent_id']??null;
-        //     $folder->name = $request['name'];
-        //     $folder->is_active = $request['is_active']??true;
-        //     $folder->save();
-        //     return ApiResponse::success($folder, 'Success', 200, $request_time);
+        $request = $request->validated();
+        try {
+            $folder = new Folder();
+            $folder->parent_id = $request['parent_id']??null;
+            $folder->name = $request['name'];
+            $folder->is_active = $request['is_active']??true;
+            $folder->save();
+            $folder=Folder::with('subFolder')->where(['parent_id'=> null,])->where('is_active', true)->orderBy('id')->get();
+            return ApiResponse::success($folder, 'Success', 200, $request_time);
 
-        // } 
-        // catch (\Exception $e) {
-        //     return $e->getMessage();
-        //     return ApiResponse::serverException([], 'Something Went Wrong !', 501, $request_time);
-        // }
-        $folder = new Folder();
-        $folder->parent_id = $request['parent_id']??null;
-        $folder->name = $request['name'];
-        $folder->is_active = $request['is_active']??true;
-        $folder->save();
+        } 
+        catch (\Exception $e) {
+            return $e->getMessage();
+            return ApiResponse::serverException([], 'Something Went Wrong !', 501, $request_time);
+        }
+
         return ApiResponse::success($folder, 'Success', 200, $request_time);
     }
 
@@ -67,7 +64,6 @@ class FolderController extends Controller
     public function show(string $id)
     {
         $request_time = date('y-m-d h:i:s');
-        $requestData = $request->validated();
         try {
             $data=Folder::findorFail($id);
             return ApiResponse::success($data, 'Success', 200, $request_time);
@@ -76,32 +72,38 @@ class FolderController extends Controller
         catch (\Exception $e) {
             return ApiResponse::serverException([], 'Something Went Wrong !', 501, $request_time);
         }
+       
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(FolderRequest $request,string $id)
     {
-        $request_time = date('y-m-d h:i:s');
-        $requestData = $request->validated();
-        try {
-            $data=Folder::find($id);
-            $data->name='';
-            return ApiResponse::success($data, 'Success', 200, $request_time);
 
-        } 
-        catch (\Exception $e) {
-            return ApiResponse::serverException([], 'Something Went Wrong !', 501, $request_time);
-        }
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(FolderRequest $request, string $id)
     {
-        //
+        $request_time = date('y-m-d h:i:s');
+        $requestData = $request->validated();
+        try {
+            $folder = Folder::find($id);
+            $folder->parent_id = $request['parent_id']?? null;
+            $folder->name = $request['name'];
+            $folder->is_active = $request['is_active']??true;
+            $folder->save();
+            $folder=Folder::with('subFolder')->where(['parent_id'=> null,])->where('is_active', true)->orderBy('id')->get();
+            return ApiResponse::success($folder, 'Success', 200, $request_time);
+
+        } 
+        catch (\Exception $e) {
+            return $e->getMessage();
+            return ApiResponse::serverException([], 'Something Went Wrong !', 501, $request_time);
+        }
     }
 
     /**
@@ -109,6 +111,17 @@ class FolderController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $request_time = date('Y-m-d H:i:s');
+        $folder=Folder::find($id);
+        try {
+            if($folder->parent_id ==null){
+                Folder::where('parent_id',$folder->id)->delete();
+            }
+            $folder->delete();
+            $folder=Folder::with('subFolder')->where(['parent_id'=> null,])->where('is_active', true)->orderBy('id')->get();
+            return ApiResponse::success($folder, 'Success', 200, $request_time);
+        } catch (\Exception $e) {
+            return ApiResponse::serverException([], 'Something Went Wrong !', 501, $request_time);
+        }
     }
 }
